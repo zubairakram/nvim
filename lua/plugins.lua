1912a13cf5e153a4c7fdd34df237efa62d273bc9
Zubair Akram <zubair616@gmail.com>
2022-02-10 09:54:28 +0500

    neogit plugin installed and indentation corrected

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,94 +1,97 @@
 -- plugins for neovim
 return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-  -- dependencies
-  use 'nvim-lua/plenary.nvim'
+    -- dependencies
+    use 'nvim-lua/plenary.nvim'
 
-  -- plugins
-  use 'leafoftree/vim-svelte-plugin'
-  use 'mattn/emmet-vim'
-  use 'mg979/vim-visual-multi'
-  use 'tpope/vim-commentary'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-surround'
-  use 'yuttie/comfortable-motion.vim'
-  use 'Yggdroot/indentLine'
-  use {
-    'windwp/nvim-autopairs',
-    require('nvim-autopairs').setup()
-  }
-  use {
-    'kyazdani42/nvim-tree.lua',
-    require('nvim-tree').setup {}
-  }
-  use {
-    'lewis6991/gitsigns.nvim',
-    require('gitsigns').setup()
-  }
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    require('nvim-treesitter.configs').setup {
-      ensure_installed = "maintained",
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      }
+    -- plugins
+    use 'leafoftree/vim-svelte-plugin'
+    use 'mattn/emmet-vim'
+    use 'mg979/vim-visual-multi'
+    use 'tpope/vim-commentary'
+    --  use 'tpope/vim-fugitive'
+    use 'tpope/vim-surround'
+    use 'yuttie/comfortable-motion.vim'
+    use 'Yggdroot/indentLine'
+    use { 'TimUntersberger/neogit',
+        require('neogit').setup{}
     }
-  }
-  use {
-    'neovim/nvim-lspconfig',
-    'williamboman/nvim-lsp-installer',
-    require("nvim-lsp-installer").on_server_ready(function(server)
-      local opts = {}
-      if server.name == "sumneko_lua" then
-        opts = {
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = {'vim', 'use'}
-              }
+    use {
+        'windwp/nvim-autopairs',
+        require('nvim-autopairs').setup()
+    }
+    use {
+        'kyazdani42/nvim-tree.lua',
+        require('nvim-tree').setup {}
+    }
+    use {
+        'lewis6991/gitsigns.nvim',
+        require('gitsigns').setup()
+    }
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        require('nvim-treesitter.configs').setup {
+            ensure_installed = "maintained",
+            highlight = {
+                enable = true,
+            },
+            indent = {
+                enable = true,
             }
-          }
         }
-      end
-      server:setup(opts)
-    end)
-  }
-  use {
-    'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-path',
-    'hrsh7th/cmp-cmdline',
-    'hrsh7th/nvim-cmp',
-    'hrsh7th/cmp-vsnip',
-    'hrsh7th/vim-vsnip'
-  }
-  use {
-    'nvim-telescope/telescope.nvim',
-    require('telescope').setup{
-      defaults = {
-        file_ignore_patterns = {
-          "node_modules"
+    }
+    use {
+        'neovim/nvim-lspconfig',
+        'williamboman/nvim-lsp-installer',
+        require("nvim-lsp-installer").on_server_ready(function(server)
+            local opts = {}
+            if server.name == "sumneko_lua" then
+                opts = {
+                    settings = {
+                        Lua = {
+                            diagnostics = {
+                                globals = {'vim', 'use'}
+                            }
+                        }
+                    }
+                }
+            end
+            server:setup(opts)
+        end)
+    }
+    use {
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-cmdline',
+        'hrsh7th/nvim-cmp',
+        'hrsh7th/cmp-vsnip',
+        'hrsh7th/vim-vsnip'
+    }
+    use {
+        'nvim-telescope/telescope.nvim',
+        require('telescope').setup{
+            defaults = {
+                file_ignore_patterns = {
+                    "node_modules"
+                }
+            }
         }
-      }
     }
-  }
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-    require('lualine').setup{
-      options = {
-        theme = 'powerline',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-      },
-      tabline = {
-        lualine_a = {'buffers'}
-      },
-      extentions = { }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+        require('lualine').setup{
+            options = {
+                theme = 'powerline',
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
+            },
+            tabline = {
+                lualine_a = {'buffers'}
+            },
+            extentions = { }
+        }
     }
-  }
 end)

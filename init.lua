c278c57fb0662636181353b32527571f5acbed33
Zubair Akram <zubair616@gmail.com>
2023-03-26 20:05:14 +0500

    lsp config updated, nvim-dap-ui installed

--- init.lua
+++ init.lua
@@ -49,6 +49,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 
     -- plugins without configuration
+    'folke/neodev.nvim',
     'karb94/neoscroll.nvim',
     'lukas-reineke/indent-blankline.nvim',
     'mattn/emmet-vim',
@@ -71,6 +72,12 @@ require("lazy").setup({
         }
     },
     {
+        'rcarriga/nvim-dap-ui',
+        dependencies = {
+            'mfussenegger/nvim-dap'
+        }
+    },
+    {
         'nvim-lualine/lualine.nvim',
         opts = {
             options = {
@@ -78,7 +85,7 @@ require("lazy").setup({
                 section_separators = { left = '', right = '' },
             },
             tabline = {
-                lualine_a = {'buffers'}
+                lualine_a = { 'buffers' }
             }
         }
     },
@@ -97,7 +104,7 @@ require("lazy").setup({
                 }
             }
         },
-        version='*',
+        version = '*',
         dependencies = {
             'nvim-lua/plenary.nvim'
         }
@@ -106,11 +113,11 @@ require("lazy").setup({
         'lewis6991/gitsigns.nvim',
         opts = {
             signs = {
-                add = { text = '+'},
-                change = { text = '~'},
-                delete = { text = '_'},
-                topdelete = { text = '‾'},
-                changedelete = { text = '~'},
+                add = { text = '+' },
+                change = { text = '~' },
+                delete = { text = '_' },
+                topdelete = { text = '‾' },
+                changedelete = { text = '~' },
             }
         }
     },
@@ -127,6 +134,7 @@ require("lazy").setup({
     },
 }, {})
 
+vim.notify = require("notify")
 require('mason').setup()
 require('mason-lspconfig').setup()
 require('neoscroll').setup()
@@ -135,45 +143,49 @@ require('nvim-tree').setup()
 require('onedark').load()
 require('Comment').setup()
 require('nvim-treesitter.configs').setup {
-    ensure_installed = { 'css', 'javascript', 'html', 'lua', 'python'},
+    ensure_installed = { 'css', 'javascript', 'html', 'lua', 'python' },
 }
 
 ----------------------------------LSP Mappings----------------------------------
+-- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
--- Use an on_attach function to only map the following keys
+-- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = { noremap=true, silent=true, buffer=bufnr }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set('n', '<space>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
-end
-
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<space>f', function()
+            vim.lsp.buf.format { async = true }
+        end, opts)
+    end,
+})
 --------------------------------NVIM CMP CONFIGS--------------------------------
-local cmp = require'cmp'
+local cmp = require 'cmp'
 
 cmp.setup({
     snippet = {
@@ -203,8 +215,8 @@ cmp.setup({
         -- { name = 'ultisnips' }, -- For ultisnips users.
         -- { name = 'snippy' }, -- For snippy users.
     }, {
-            { name = 'buffer' },
-        })
+        { name = 'buffer' },
+    })
 })
 
 -- Set configuration for specific filetype.
@@ -212,8 +224,8 @@ cmp.setup.filetype('gitcommit', {
     sources = cmp.config.sources({
         { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
     }, {
-            { name = 'buffer' },
-        })
+        { name = 'buffer' },
+    })
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
@@ -230,9 +242,19 @@ cmp.setup.cmdline(':', {
     sources = cmp.config.sources({
         { name = 'path' }
     }, {
-            { name = 'cmdline' }
-        })
+        { name = 'cmdline' }
+    })
 })
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+local servers = { 'lua_ls', 'tsserver', 'pyright' }
+for key, value in pairs(servers) do
+    require('lspconfig')[value].setup {
+        capabilities = capabilities
+    }
+end
 
 -- treesitter configuration
 require('nvim-treesitter.configs').setup {
@@ -241,11 +263,9 @@ require('nvim-treesitter.configs').setup {
     },
     indent = {
         enable = true,
-        disable = {'python'}
+        disable = { 'python' }
     }
 }
-
-vim.notify = require("notify")
 
 -- enable nvim-tree indent_markers
 require('nvim-tree').setup({
@@ -258,13 +278,3 @@ require('nvim-tree').setup({
 
 -- friendly snippets plugin loaded
 require("luasnip.loaders.from_vscode").lazy_load()
-
--- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-local servers = {'lua_ls', 'tsserver', 'pyright'}
-for key, value in pairs(servers) do
-    require('lspconfig')[value].setup {
-        capabilities = capabilities
-    }
-end
